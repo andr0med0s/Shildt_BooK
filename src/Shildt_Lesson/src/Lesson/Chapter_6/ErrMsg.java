fe1322655be21833e0213fc924b37b599d26823f
andr0med0s <andromedoz@gmail.com>
2023-02-15 09:49:12 +0500

    Chapter_6 Returning objects by methods

---
+++ src/Shildt_Lesson/src/Lesson/Chapter_6/ErrMsg.java
@@ -0,0 +1,78 @@
+package Shildt_Lesson.src.Lesson.Chapter_6;
+
+//Возврат объектов методами стр 232
+
+//Метод может возвращать данные любого типа, включая и типы классов.
+
+// Возврат объекта типа String
+class ErrorMsg {
+    String[] msgs = {
+            "Ошибка вывода",
+            "Ошибка ввода",
+            "Отсутствует место на диске",
+            "Выход индекса за границы диапазона"
+    };
+    // Возврат сообщения об ошибке
+    String getErrorMsg (int i) { //◄ -------------Возврат объекта типа String
+        if (i >= 0 & i < msgs.length)
+            return msgs[i];
+        else
+            return "Несуществующий код ошибки";
+    }
+}
+
+class ErrMsg {
+    public static void main(String[] args) {
+        ErrorMsg err = new ErrorMsg();
+        System.out.println(err.getErrorMsg(2));
+        System.out.println(err.getErrorMsg(19));
+    }
+}
+
+// Возврат объекта, определяемого разработчиком программы
+class Err {
+    String msg;// сообщение об ошибке
+    int severity; // уровень серьезности ошибки
+    Err(String m, int s) {
+        msg = m;
+        severity = s;
+    }
+}
+class ErrorInfo {
+    String[] msgs = {
+            "Ошибка вывода",
+            "Ошибка ввода",
+            "Отсутствует место на диске",
+            "Выход индекса за границы диапазона"
+    };
+    int[] howbad = { 3, 3, 2, 4 };
+
+    Err getErrorInfo (int i) { //◄------Возврат объекта типа Err
+        if (i >= 0 & i < msgs.length)
+            return new Err(msgs[i], howbad[i]);
+        else
+            return new Err("Несуществующий код ошибки", 0);
+    }/*
+        При каждом вызове метода getErrorlnfo() создается новый объект типа
+        Err, и ссылка на него возвращается вызывающему методу. Затем этот объект
+        используется методом main () для отображения степени серьезности ошибки и
+        текстового сообщения.
+    */
+}
+class ErrInfo {
+    public static void main(String args[]) {
+        ErrorInfo err = new ErrorInfo();
+        Err e;
+        e = err.getErrorInfo(2);
+        System.out.println(e.msg + " уровень: " + e.severity);
+        e = err.getErrorInfo(19);
+        System.out.println(e.msg + " уровень: " + e.severity);
+    }
+}
+/*
+* Объект, возвращенный методом, существует до тех пор, пока на него имеется
+хотя бы одна ссылка. Если ссылки на объект отсутствуют, он уничтожается под-
+системой сборки мусора. Поэтому при выполнении программы не может воз-
+никнуть ситуация, когда объект удаляется лишь потому, что метод, в котором
+он был создан, завершился.
+* */
